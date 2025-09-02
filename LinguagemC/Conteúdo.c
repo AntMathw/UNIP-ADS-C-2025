5f7eb059db97f5183690743be87e359fbbc24c97
Theuzin <mateusetec424@gmail.com>
2025-09-02 20:20:12 -0300

    Update Conteúdo.c

--- LinguagemC/Conteúdo.c
+++ LinguagemC/Conteúdo.c
@@ -241,6 +241,23 @@ int main(){
     return 0;
 }
 
+///Recursividade Fatorial
+
+#include <stdio.h>
+
+int fatorial(int n) {
+    if (n == 0 || n == 1)  // Caso base
+        return 1;
+    else
+        return n * fatorial(n - 1);  
+}
+
+int main() {
+    int num = 6;
+    printf("Fatorial de %d é %d\n", num, fatorial(num));
+    return 0;
+}
+
 
 
 
