5d63780e128df3a39a443235981def987b7ad524
Theuzin <mateusetec424@gmail.com>
2025-09-23 17:03:47 -0300

    Create If, Else if e Else.c

---
+++ LinguagemC/Estrutura Condicionais/If, Else if e Else.c
@@ -0,0 +1,10 @@
+ #include <stdio.h>
+ int main() {
+    int a = 3;
+    if (a > 5) {
+        printf("a é maior que 5\n");
+    } else {
+        printf("a não é maior que 5\n");
+    }
+    return 0;
+ 
