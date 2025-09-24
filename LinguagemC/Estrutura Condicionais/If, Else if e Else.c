af0c534fd5cb9fc44499feb485c0e9b8e61a873c
Theuzin <mateusetec424@gmail.com>
2025-09-24 12:13:47 -0300

    Update If, Else if e Else.c

--- LinguagemC/Estrutura Condicionais/If, Else if e Else.c
+++ LinguagemC/Estrutura Condicionais/If, Else if e Else.c
@@ -8,3 +8,22 @@
     }
     return 0;
  
+
+#include <stdio.h>
+
+int main() {
+    int numero;
+
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &numero);
+
+    if (numero > 0) {
+        printf("O numero e positivo.\n");
+    } else if (numero < 0) {
+        printf("O numero e negativo.\n");
+    } else {
+        printf("O numero e zero.\n");
+    }
+
+    return 0;
+}
