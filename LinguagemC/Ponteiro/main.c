cc43b032e85f40483be349a32371c6ff9aad6696
Theuzin <mateusetec424@gmail.com>
2025-10-07 20:32:40 -0300

    Update main.c

--- LinguagemC/Ponteiro/main.c
+++ LinguagemC/Ponteiro/main.c
@@ -1,1 +1,18 @@
+//Ponteiro básico
 
+#include <stdio.h>
+
+int main(){
+    
+    int var = 10;
+    
+    int *ptr;
+    
+    ptr = &var;
+    
+    printf("var = %d[%p]\n\n", var, (void*)&var);
+    printf("*ptr = %d[%p][%p]\n", *ptr, (void*)ptr,(void*)&ptr);
+    
+    
+    return 0;
+}
