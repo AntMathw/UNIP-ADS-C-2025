61651e450d49fc0bc2493fae5078198ee647d8f9
Theuzin <mateusetec424@gmail.com>
2025-10-14 20:40:57 -0300

    Update main.c

--- LinguagemC/Ponteiro/Alocação de Memória/main.c
+++ LinguagemC/Ponteiro/Alocação de Memória/main.c
@@ -13,3 +13,37 @@ int main() {
     // Exemplo: preencher o vetor com valores de 1 a 10
     for (int i = 0; i < 10; i++) {
         p
+//SOMA DOS ELEMENTOS DE ALOCAÇÃO DE MEMÓRIA
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    
+    int t;
+    printf("Informe o tamanho do vetor: ");
+    scanf("%d", &t);
+    
+    if(t > 0){
+        int * ptr = (int*) malloc(t * sizeof(int));
+        for(int i = 0; i < t; i++){
+            printf("Informe o valor da posição %d: ", i);
+            scanf("%d", &ptr[i]);
+            
+        }
+        int soma = 0;
+        for(int i = 0; i < t; i++){
+            soma += ptr[i];
+        }
+        printf("A soma dos valores informados é %d", soma);
+        free(ptr);
+        ptr = NULL;
+    }else{
+        printf("O valor informado deve ser maior que 0");
+        
+    }
+    
+    return 0;    
+        
+    
+}
