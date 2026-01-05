02b848c1acc4f7432512a4fae42347e8227534d2
Mateus Antunes <mateusetec424@gmail.com>
2026-01-05 16:40:07 -0300

    Update System cadastro.c

--- LinguagemC/System cadastro.c
+++ LinguagemC/System cadastro.c
@@ -4,7 +4,6 @@
 #include <string.h> 
 #include <ctype.h>  
 
-// Compatibilidade para o "Sleep" funcionar em Windows e Linux
 #ifdef _WIN32
     #include <windows.h>
     #define SLEEP_MS(ms) Sleep(ms)
@@ -13,7 +12,6 @@
     #define SLEEP_MS(ms) usleep(ms * 1000)
 #endif
 
-// Função para deixar a primeira letra maiúscula (Equivalente ao .capitalize())
 void formatar_nome(char str[]) {
     if (str[0] != '\0') {
         str[0] = toupper(str[0]);
@@ -24,24 +22,34 @@ void formatar_nome(char str[]) {
 }
 
 int main() {
-    char nome[50], sobrenome[50];
+    // Inicializando com "" para garantir que a memória esteja limpa
+    char nome[50] = "";
+    char sobrenome[50] = "";
 
-    printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
-    printf("                                        \n");
-    printf("                BEM VINDO AO SISTEMA                             \n");
-    printf("                          DE                 \n");
-    printf("                CADASTRO DE USUARIO          \n");
-    printf("                                         \n");
-    printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
-    SLEEP_MS(3000);
+    // Ajustei o cabeçalho para ficar simétrico
+    printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+    printf("|                                         |\n");
+    printf("|          BEM VINDO AO SISTEMA           |\n");
+    printf("|                   DE                    |\n");
+    printf("|           CADASTRO DE USUARIO           |\n");
+    printf("|                                         |\n");
+    printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+    SLEEP_MS(2000);
 
-    printf("Ola usuario, entao vamos comecar o seu cadastro!\n");
-    printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
-    SLEEP_MS(3000);
+    printf("\nOla usuario, entao vamos comecar o seu cadastro!\n");
+    printf("-------------------------------------------\n");
+    SLEEP_MS(2000);
 
     printf("Primeiramente gostaria de saber seu nome e sobrenome!\n");
-    printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
-    SLEEP_MS(3000);
+    printf("-------------------------------------------\n");
+    SLEEP_MS(2000);
+
+    // Exemplo de como ler com segurança:
+    printf("Digite seu nome: ");
+    scanf("%s", nome); 
+    formatar_nome(nome);
+
+    printf("Nome registrado: %s\n", nome);
 
     return 0;
 }
